0a4cf22e063d4db6d474358afe87160820b1cf17
JakubDrobnik <drobnik.j@gmail.com>
2017-11-12 18:44:42 +0100

    move files

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+MONGO_URL='mongodb://trending-repos:hXoGqwF4UWSP8s2w@85.90.244.79:27017/trending-repos?authSource=admin' USER_ID='RWnGtczwPrB3uTMak' TOKEN='f5J7XnWJ8LDyRywwnSznuGGo9' node index.js
