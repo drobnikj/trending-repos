78fdb2aa8771b56ab3826f1cf6864030d6fd0ef0
JakubDrobnik <drobnik.j@gmail.com>
2018-02-14 12:37:04 +0530

    improvements

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-MONGO_URL='mongodb://trending-repos:hXoGqwF4UWSP8s2w@85.90.244.79:27017/trending-repos?authSource=admin' USER_ID='RWnGtczwPrB3uTMak' TOKEN='f5J7XnWJ8LDyRywwnSznuGGo9' node index.js
